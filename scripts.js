e42add4c24af7088f6343bba17daee1331fa682c
floridots <felipeffloriani@gmail.com>
2023-03-26 00:14:33 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const form = document.getElementById("budgetForm");
 const viewsInput = document.getElementById("views");
 const adsenseSelect = document.getElementById("adsense");
 const resultDiv = document.getElementById("result");
+const optionInfoDiv = document.querySelector(".option-info");
 
 // Define adsense cost per view
 const ADS_COSTS = {
@@ -30,6 +31,36 @@ function displayResult() {
     }
     resultDiv.classList.add("result-shown");
   }
+
+// Function to update option info
+function updateOptionInfo() {
+  const adsense = adsenseSelect.value;
+  let optionText = "";
+  
+  switch (adsense) {
+    case "50":
+      optionText = "No Ads: Funciona em Nuvem, não precisa ser instalado na máquina do cliente, funciona 24/7, porém não visualiza anúncios da Twitch.";
+      break;
+    case "250":
+      optionText = "Medium Ads: Funciona em Nuvem, não precisa ser instalado na máquina do cliente, funciona 24/7, assiste todos os adsense e contas da twitch conectada para maior segurança.";
+      break;
+    case "500":
+      optionText = "High Ads: Funciona em Nuvem, não precisa ser instalado na máquina do cliente, funciona 24/7, assiste todos os adsense e contas da twitch conectadas, chatbot incluso e o bot abre o site dos anuncios e simulam compras e registro de email que ira gerar uma renda de até 6x a mais para o cliente.";
+      break;
+    default:
+      optionText = "Selecione uma opção para ver a descrição.";
+  }
+
+  optionInfoDiv.textContent = optionText;
+} 
+
+// Add event listener to adsenseSelect
+adsenseSelect.addEventListener("change", () => {
+  updateOptionInfo();
+});
+
+// Initialize option info
+updateOptionInfo();
   
 
 // Add event listener to form submit
@@ -59,3 +90,14 @@ viewsInput.addEventListener("input", () => {
     localStorage.setItem("adsense", adsenseSelect.value);
     displayResult();
   });
+
+  // Get DOM elements for nav links
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Add event listeners for smooth scrolling
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    // Add smooth scrolling behavior here
+  });
+});
